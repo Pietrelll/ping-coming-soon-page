ed492753e685b162f22fb7c3faaa642d8b1db658
Pietrell <piotrplotast2006@gmail.com>
2023-08-31 23:18:02 +0200

    fixed red border to show up only if email invalid

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const emailField = document.querySelector('#email');
 const errorField = document.querySelector('#error')
-
-
 function validateEmail() {
     if (!emailField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
         errorField.innerHTML = "Please provide a valid email address"
@@ -9,6 +7,7 @@ function validateEmail() {
         errorField.style.marginBottom = "1rem";
         errorField.style.marginTop = "0.5rem";
         errorField.style.gridArea = "c";
+        emailField.style.border = "1px solid hsl(354, 100%, 66%)";
         return false;
     }
     errorField.innerHTML = "";
