fb3ca30bc30652d1aafaf57f2d7b1ca00fdb92b7
Pietrell <piotrplotast2006@gmail.com>
2023-08-31 22:14:24 +0200

    fixed typo in address

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const errorField = document.querySelector('#error')
 
 function validateEmail() {
     if (!emailField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
-        errorField.innerHTML = "Please provide a valid email adress"
+        errorField.innerHTML = "Please provide a valid email address"
         errorField.style.fontSize = "0.7rem";
         errorField.style.marginBottom = "1rem";
         errorField.style.marginTop = "0.5rem";
